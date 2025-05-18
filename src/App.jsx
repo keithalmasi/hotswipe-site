911104cfc30877579718828ab627a2cc0c7d265e
keithalmasi <kwilso17@gmail.com>
2025-05-18 07:18:36 -0400

    Fix: case-sensitive imports for Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import LandingPage from './components/LandingPage';
+import LandingPage from './Components/LandingPage';
 import {
   ConnectionProvider,
   WalletProvider
